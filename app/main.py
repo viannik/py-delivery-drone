d59fd67bf78b2d83aebafb44898ef940835942d2
viannik <ivan.nikitski@gmail.com>
2025-06-28 01:14:14 +0300

    Simplify default coordinate handling in robot class constructors

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -34,7 +34,7 @@ class FlyingRobot(BaseRobot):
     def __init__(
         self, name: str, weight: int, coords: List[int] = None
     ) -> None:
-        coords = coords if coords is not None else [0, 0, 0]
+        coords = coords or [0, 0, 0]
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
